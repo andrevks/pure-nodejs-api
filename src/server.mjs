ae63ef3163d85e999264f4e527bd54ecfbfbc574
andrevks <andregp019@gmail.com>
2023-07-05 21:45:28 -0400

    feature: import csv file to create tasks

--- src/server.mjs
+++ src/server.mjs
@@ -1,12 +1,9 @@
 import http from 'node:http'
 import { routes } from './routes.mjs'
-import { jsonMiddleware } from './middlewares/json-middleware.mjs'
 import { extractQueryParams } from './utils/extract-query-params.mjs'
 
 const server = http.createServer(async (req, res) => {
   const { method, url } = req
-
-  await jsonMiddleware(req, res)
 
   const route = routes.find(route => {
     return route.method === method && route.path.test(url)
